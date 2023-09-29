43899e76445d90d1803cb07dc26d99e617f1639e
KhushiSinghania <khushisinghaniaktr@gmail.com>
2023-09-30 02:11:00 +0530

    finalcommit

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# QuickNotes
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+<br />
+A basic React CRUD Application made for storing User Data and Notes in a remote database and fetch data and display notes giving user various options. It also include an email based authentication system which seprates notes that are created by different users.  
+
+
+
+## Features
+
+- CRUD Notes
+- Secure - Email based authentication
+- Responsive UI
+
+
+## Installation
+
+
+1. Clone repository
+
+
+
+2. Install packages
+```bash
+npm install
+```
+
+3. Start the development server:
+
+```bash
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import UserDetail from "./components/UserDetail";
+
+function App() {
+    const [alert, setAlert] = useState(null);
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type,
+        });
+        setTimeout(() => {
+            setAlert(null);
+        }, 3000);
+    };
+    return (
+        <div>
+            <NoteState>
+                <Router>
+                    <Navbar />
+                    <div className="container">
+                        <Alert alert={alert} />
+                        <Routes>
+                            <Route
+                                exact
+                                path="/"
+                                element={<Home showAlert={showAlert} />}
+                            />
+                            <Route exact path="/about" element={<About />} />
+                            <Route
+                                exact
+                                path="/login"
+                                element={<Login showAlert={showAlert} />}
+                            />
+                            <Route
+                                exact
+                                path="/signup"
+                                element={<Signup showAlert={showAlert} />}
+                            />
+                            <Route
+                                exact
+                                path="/userdetail"
+                                element={<UserDetail />}
+                            />
+                        </Routes>
+                    </div>
+                </Router>
+            </NoteState>
+        </div>
+    );
+}
+
+export default App;
